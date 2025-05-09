2e5f8670881bfb4633dbb2c733e54c2b7832badf
shour0 <shour908@gmail.com>
2025-05-09 18:38:33 +0300

    using partial pre-rendering

---
+++ app/(root)/startup/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { STARTUP_BY_ID_QUERY } from '@/lib/queries'
+import { client } from '@/sanity/lib/client'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+export const experemental_ppr = true
+
+const Page = async ({ params }: { params: Promise<{ id: string}>}) => {
+  const id = (await params).id
+
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  if (!post) return notFound()
+  return <>
+  <h1 className='text-3xl'>{post.title}</h1>
+  </>
+}
+
+export default Page
--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,15 @@ const nextConfig: NextConfig = {
         hostname: '*', 
       }
     ]
+  },
+  experimental: {
+    ppr: 'incremental'
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right"
   }
-
 };
 
 export default nextConfig;
