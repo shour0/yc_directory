7035005839db7cc6cccdb8928819dfbb6200869c
shour0 <shour908@gmail.com>
2025-05-20 19:29:33 +0300

    added author authentication flow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,17 @@ import AnimatedHero from "@/components/AnimatedHero";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({searchParams}:
    {searchParams: Promise<{query?:string}>}) {
     const query = (await searchParams).query; 
     const params = { search: query || null}
+
+    const session = await auth()
+
+    console.log(session?.id);
+    
     const { data:posts } = await sanityFetch({query: STARTUPS_QUERY,params}) 
 
     console.log(JSON.stringify(posts, null, 2))
